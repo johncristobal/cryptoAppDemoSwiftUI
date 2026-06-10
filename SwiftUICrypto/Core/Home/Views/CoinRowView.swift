3c1b82a9a1f2dd98bd5caed69ff8491810572ba3
johhcristobal <johnalexiscristobaljimenez6@gmail.com>
2026-06-10 16:15:00 -0600

    coin view model, environment data, animation transition

---
+++ SwiftUICrypto/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinRowView.swift
+//  SwiftUICrypto
+//
+//  Created by JOHN CRIS on 09/06/26.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinRowView()
+}
